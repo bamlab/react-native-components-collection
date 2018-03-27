6239d77d7fcf3a8f6a079395ef71e51e7de98cf2
Alban Depretz <32538485+chdeps@users.noreply.github.com>
2018-03-27 18:40:58 +0200

    Update index.js

--- packages/react-native-component-text-input/index.js
+++ packages/react-native-component-text-input/index.js
@@ -40,7 +40,7 @@ export default class TextInput extends Component {
       const Touchable = PasswordVisibilityTouchable || TouchableOpacity;
       return (
         <View style={[styles.textInputContainer, containerStyle]}>
-          <TextInput style={[styles.textInput, style]} {...passwordProps} {...rest} />
+          <TextInput ref={this._setRef} style={[styles.textInput, style]} {...passwordProps} {...rest} />
           {!!renderSecureTextEntryVisibilityButtonContent && (
             <Touchable
               style={[styles.secureTextEntryVisibilityButton, passwordVisibilityButtonStyle]}
