acccbbbe2767c4771dc48ba44e5eeadba1594ce7
Alban Depretz <alband@bam.tech>
2018-04-02 12:59:29 +0200

    Add support for custom ActicityIndicator, Logo..

--- packages/react-native-component-google-signin/index.js
+++ packages/react-native-component-google-signin/index.js
@@ -1,6 +1,14 @@
 import React, { PureComponent } from 'react';
-import { GoogleSignin } from 'react-native-google-signin';
-import { Image, StyleSheet, TouchableOpacity, View, Text, ActivityIndicator } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  ActivityIndicator as RNActivityIndicator,
+} from 'react-native';
+import { GoogleSignIn } from 'react-native-google-signin';
+import DefaultGoogleLogo from './lib/images/google_logo.png';
 
 export default class GoogleAuthButton extends PureComponent {
   state = {
@@ -9,30 +17,46 @@ export default class GoogleAuthButton extends PureComponent {
 
   _loginWithGoogle = () => {
     const { googleSignInConfig, onPress } = this.props;
+    if (!GoogleSignIn) {
+      console.warn('GoogleSignIn is not installed');
+      return;
+    }
     this.setState({ isLoading: true });
-    GoogleSignin.configure(googleSignInConfig)
-      .then(() => GoogleSignin.signIn())
+    GoogleSignIn.configure(googleSignInConfig)
+      .then(() => GoogleSignIn.signIn())
       .then(token => onPress(token))
-      .catch(e => console.log(e))
-      .finally(() => this.setState({ isLoading: false }));
+      .catch(e => console.warn(e))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
   };
 
   render() {
-    const { buttonStyle, textStyle, activityIndicatorColor, googleLogo, isLoading } = this.props;
+    const {
+      buttonStyle,
+      textStyle,
+      text,
+      activityIndicatorColor,
+      CustomGoogleLogo,
+      ActivityIndicatorComponent,
+      ...rest
+    } = this.props;
+    const ActivityIndicator = ActivityIndicatorComponent || RNActivityIndicator;
+    const GoogleLogo = CustomGoogleLogo || DefaultGoogleLogo;
+    const isLoading = this.props.isLoading || this.state.isLoading;
     return (
       <TouchableOpacity
         activeOpacity={0.7}
         style={[styles.container, buttonStyle]}
-        disabled={this.state.isLoading}
-        onPress={this._loginWithGoogle}>
-        {!!googleLogo && (
-          <Image source={this.props.googleLogo} resizeMode="contain" style={styles.icon} />
-        )}
+        disabled={isLoading}
+        onPress={this._loginWithGoogle}
+        {...rest}>
+        {!!GoogleLogo && <Image source={GoogleLogo} resizeMode="contain" style={styles.icon} />}
         <View style={styles.textContainer}>
-          {this.state.isLoading || isLoading ? (
+          {isLoading ? (
             <ActivityIndicator color={activityIndicatorColor} />
           ) : (
-            <Text style={textStyle}>{this.props.text}</Text>
+            <Text style={textStyle}>{text}</Text>
           )}
         </View>
       </TouchableOpacity>
