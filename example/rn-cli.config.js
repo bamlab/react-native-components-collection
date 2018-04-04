adc9e55b34f84b1bd55e6f63b09a2e5565197018
Louis Lagrange <lagrange.louis@gmail.com>
2018-04-04 19:58:43 +0200

    Add blacklist

--- example/rn-cli.config.js
+++ example/rn-cli.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const glob = require('glob-to-regexp');
+const blacklist = require('metro/src/blacklist');
+
 module.exports = {
   getProjectRoots() {
     return [__dirname, path.resolve(__dirname, '../packages/react-native-component-text')];
@@ -8,5 +11,11 @@ module.exports = {
       'react-native',
       'react',
     ];
+  },
+  getBlacklistRE() {
+    return blacklist([
+      glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+      glob(`${path.resolve(__dirname, '..')}/packages/react-native-component-text/node_modules/*`),
+    ]);
   },
 };
