1e6718ec46641617c8c35210e87eb968e02561bb
Alban Depretz <alband@bam.tech>
2018-04-13 09:31:36 +0200

    Remove Google SignIn from the stories

--- stories/GoogleSignIn.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import GoogleSignInButton from '../packages/react-native-component-google-signin';
-
-storiesOf('GoogleSignIn', module).add('default', () => <GoogleSignInButton text="GOOGLE" />);
