8124a3e78c9469f8c59995b6ce8f631e7a49dc48
Louis Lagrange <lagrange.louis@gmail.com>
2018-07-14 10:09:20 +0200

    Add storybook addon to see source
    (closes #10)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+      },
+      {
         test: /\.js$/,
         include: [
           /node_modules\/react-native-/,
